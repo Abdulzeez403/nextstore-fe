1e02653c3ceb2ed9281da5631a1b099129d529a3
abdulazeezsodiq <sodiq.asynctechs@gmail.com>
2025-02-07 16:12:39 +0100

    new update fixing the bug

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,13 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack: (config, { isServer }) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      punycode: require.resolve("punycode/"),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
